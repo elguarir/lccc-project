38aeffd86edf157d58625de36b0796a891c15195
MohamedElguarir <120427922+MohamedElguarir@users.noreply.github.com>
2023-11-23 23:46:26 +0000

    quick fix

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,17 +1,12 @@
-import { Await } from "@/components/shared/Await";
 import { currentUser } from "@clerk/nextjs";
 
-function DashboardPage() {
-  const user = currentUser();
+async function DashboardPage() {
+  const user = await currentUser();
   return (
     <div className="flex flex-col flex-1 w-full py-6">
-      <Await promise={user}>
-        {(user) => (
-          <h1 className="mb-8 text-2xl font-bold md:text-3xl text-foreground">
-            Howdy, <span className="text-primary-600">{user?.firstName} 👋</span>
-          </h1>
-        )}
-      </Await>
+      <h1 className="mb-8 text-2xl font-bold md:text-3xl text-foreground">
+        Howdy, <span className="text-primary-600">{user?.firstName} 👋</span>
+      </h1>
       {/* <h1 className="text-3xl mb-8 font-[550] md:font-bold font-display text-foreground">
         Dashboard
       </h1> */}
