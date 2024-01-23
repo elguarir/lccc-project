1064e0464568d0959b679749d5683554aa389e47
MohamedElguarir <120427922+MohamedElguarir@users.noreply.github.com>
2024-01-23 02:36:04 +0100

    fix

--- src/app/(protected)/dashboard/members/page.tsx
+++ src/app/(protected)/dashboard/members/page.tsx
@@ -1,10 +1,9 @@
 import AddNewUser from "@/components/dashboard/members/AddNew";
 import MembersTable from "@/components/dashboard/members/MembersTable";
-import { trpcVanilla } from "@/server/client";
 import { getUsersList } from "@/server/routers/user";
 
 async function MembersPage() {
-  let formattedUsers = await trpcVanilla.user.getUsersList.query()
+  let formattedUsers = await getUsersList();
   return (
     <main className="flex flex-col items-center w-full py-3 md:py-5">
       <header className="flex items-center justify-between w-full">
