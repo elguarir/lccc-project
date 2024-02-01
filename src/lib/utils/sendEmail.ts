3adb38391704726d61703f341602ea2c726554a0
MohamedElguarir <120427922+MohamedElguarir@users.noreply.github.com>
2024-02-01 06:30:59 +0100

    quick fix

--- src/lib/utils/sendEmail.ts
+++ src/lib/utils/sendEmail.ts
@@ -1,13 +1,3 @@
-import { env } from "@/lib/env/server";
-import nodemailer from "nodemailer";
-import Mailgun from "mailgun.js";
-// @ts-ignore
-const mailgun = new Mailgun(FormData);
-const mg = mailgun.client({
-  username: "moha",
-  key: env.MAILGUN_API_KEY,
-});
-
 
 type SendEmailProps = {
   to: string;
@@ -16,17 +6,17 @@ type SendEmailProps = {
 };
 
 export const sendEmail = async ({ to, subject, html }: SendEmailProps) => {
-  const form = new FormData();
-  form.append("from", "Este Center <moha@elguarir.dev>");
-  form.append("to", to);
-  form.append("subject", subject);
-  form.append("html", html);
+  // const form = new FormData();
+  // form.append("from", "Este Center <moha@elguarir.dev>");
+  // form.append("to", to);
+  // form.append("subject", subject);
+  // form.append("html", html);
 
-  return await fetch("https://api.mailgun.net/v3/elguarir.dev/messages", {
-    method: "POST",
-    headers: {
-      Authorization: "Basic " + btoa(`api:${env.MAILGUN_API_KEY}`),
-    },
-    body: form,
-  });
+  // return await fetch("https://api.mailgun.net/v3/elguarir.dev/messages", {
+  //   method: "POST",
+  //   headers: {
+  //     Authorization: "Basic " + btoa(`api:${env.MAILGUN_API_KEY}`),
+  //   },
+  //   body: form,
+  // });
 };
