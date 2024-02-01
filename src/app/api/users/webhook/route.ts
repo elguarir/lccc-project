065eb8873294891f15e2671035064be9171edf30
MohamedElguarir <120427922+MohamedElguarir@users.noreply.github.com>
2024-02-01 21:24:00 +0100

    fix relation mode

--- src/app/api/users/webhook/route.ts
+++ src/app/api/users/webhook/route.ts
@@ -4,11 +4,11 @@ import { WebhookEvent } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 import { sendWelcomeEmail } from "@/server/routers/emails";
 import { PrismaClient } from "@prisma/client";
+import db from "@/prisma";
 
 export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
   const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
-  let db = new PrismaClient();
   if (!WEBHOOK_SECRET) {
     throw new Error(
       "Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local",
