3393b646e3e183cb14677409dfb3e9f6877fc580
Mohamed Elguarir <120427922+MohamedElguarir@users.noreply.github.com>
2024-02-01 07:45:24 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,13 +3,7 @@ const nextConfig = {
   images: {
     domains: ["este-center.s3.amazonaws.com", "res.cloudinary.com"],
   },
-  experimental: {
-    serverComponentsExternalPackages: [
-      "@react-email/components",
-      "@react-email/render",
-      "@react-email/tailwind",
-    ],
-  },
+  
 };
 
 export default nextConfig;
