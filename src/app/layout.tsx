4137ee79462936eb48dc3445261fa77642ccd925
MohamedElguarir <120427922+MohamedElguarir@users.noreply.github.com>
2024-02-03 05:10:58 +0100

    quick test again

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,40 +28,12 @@ interface RootLayoutProps {
   children: React.ReactNode;
 }
 const fontHeading = localFont({
-  src: [
-    {
-      path: "./fonts/CabinetGrotesk-Thin.otf",
-      weight: "100",
-    },
-    {
-      path: "./fonts/CabinetGrotesk-Light.otf",
-      weight: "300",
-    },
-    {
-      path: "./fonts/CabinetGrotesk-Regular.otf",
-      weight: "400",
-    },
-    {
-      path: "./fonts/CabinetGrotesk-Medium.otf",
-      weight: "500",
-    },
-    {
-      path: "./fonts/CabinetGrotesk-Bold.otf",
-      weight: "700",
-    },
-    {
-      path: "./fonts/CabinetGrotesk-ExtraBold.otf",
-      weight: "800",
-    },
-    {
-      path: "./fonts/CabinetGrotesk-Black.otf",
-      weight: "900",
-    },
-  ],
+  src: "./fonts/CabinetGrotesk-Variable.woff2",
   fallback: ["Cabinet Grotesk", "sans-serif"],
   display: "swap",
   variable: "--font-heading",
 });
+
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <>
